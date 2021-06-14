47a319921a3d8c45cdd3e7fa012bab11120e6025
Esewi Egheosa <esewi12@gmail.com>
2021-06-14 23:36:07 +0000

    small tweaks

--- Assets/Scripts/Volumes&Areas/BossExit.cs
+++ Assets/Scripts/Volumes&Areas/BossExit.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class BossExit : MonoBehaviour
 {
     [SerializeField] private BossType bossType;
-
+    bool isTriggered;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")&&!isTriggered)
         {
             HubManager.instance.LoadSelectedBossRoom(bossType);
+            isTriggered = true;
         }
 
     }
