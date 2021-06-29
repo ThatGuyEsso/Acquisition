dc02151b2ebb964cf6d66eb348942642dd0e7531
Esewi Egheosa <esewi12@gmail.com>
2021-06-29 09:18:37 +0000

    New folder organisation

---
+++ Assets/Scripts/Gameplay/Weapons/Projectile/BouncingProjectile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingProjectile : Base_Projectile
+{
+    [SerializeField] private int bounceCount;
+
+    
+
+
+    public void Reflect()
+    {
+
+    }
+}
